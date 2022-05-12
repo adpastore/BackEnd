55020cdba62872b2a550fae972e63bd6e02015eb
Andres Pastore <adpastore@gmail.com>
2022-05-11 22:58:22 -0300

    Inicio

---
+++ src/test/java/com/ap/ap/ApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.ap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
