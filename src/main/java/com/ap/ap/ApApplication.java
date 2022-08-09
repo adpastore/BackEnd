f25b24dbf12d971b99766ae48d07511131d2a48e
Andres Pastore <adpastore@gmail.com>
2022-08-08 21:06:54 -0300

    Update ApApplication.java

--- src/main/java/com/ap/ap/ApApplication.java
+++ src/main/java/com/ap/ap/ApApplication.java
@@ -41,7 +41,7 @@ public class ApApplication {
                 "Content-Type","Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-                "Access-Control-Allow-Origin: https://apadpfront.web.app Vary: Origin", "Access-Control-Allow-Credentials"));
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin: https://apadpfront.web.app Vary: Origin", "Access-Control-Allow-Credentials"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
