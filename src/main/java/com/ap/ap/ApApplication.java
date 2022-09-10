01fb09280e6872aaf3537a7a0b5611bcf2cea2af
Andres Pastore <adpastore@gmail.com>
2022-09-09 21:21:55 -0300

    Agregado de ultima modificacion

--- src/main/java/com/ap/ap/ApApplication.java
+++ src/main/java/com/ap/ap/ApApplication.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 
@@ -22,8 +25,8 @@ public class ApApplication {
 		    @Override
 		    public void addCorsMappings(CorsRegistry registry) {
 
-			registry.addMapping("/api/**").allowedOrigins("https://adpapfront.web.app/portfolio").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
-			registry.addMapping("/auth/**").allowedOrigins("https://adpapfront.web.app/portfolio").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+			registry.addMapping("/api/**").allowedOrigins("https://adpapfront.web.app").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+                        CorsRegistration allowedHeaders = registry.addMapping("/auth/**").allowedOrigins("https://adpapfront.web.app").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
 		    }
 		};
 	    }
@@ -31,7 +34,7 @@ public class ApApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://adpapfront.web.app/portfolio"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://adpapfront.web.app"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
